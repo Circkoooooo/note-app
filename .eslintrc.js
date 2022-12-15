7e76057551c00e134f6b9b2f0331526a860b0325
2389451262@qq.com <2389451262@qq.com>
2022-12-16 02:35:59 +0800

    initialize

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+	overrides: [],
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		},
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+				moduleDirectory: ['node_modules', 'src/'],
+			},
+		},
+		'import/extensions': 0,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/jsx-filename-extension': 'off',
+		'import/extensions': 'off',
+	},
+}
