d56263d1361a5d8760a2187bdfaa56c1f4843d2c
2389451262@qq.com <2389451262@qq.com>
2022-12-16 17:00:41 +0800

    feat: hook, use to connect siderbar and router

---
+++ src/hooks/useSider.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export type SiderRouterConfigsType = {
+	key: string
+	route: {
+		path: string
+	}
+}
+
+export default (configs: SiderRouterConfigsType[]) => {
+	const [currentKey, setCurrentKey] = useState(configs[0].key)
+
+	const navigate = useNavigate()
+	useEffect(() => {
+		const config = configs.find((item) => item.key === currentKey)
+		const path = config && config.route.path ? config.route.path : '/'
+		navigate(path)
+	}, [currentKey])
+
+	return [currentKey, setCurrentKey] as [string, typeof setCurrentKey]
+}
