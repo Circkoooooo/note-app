ed3affa73bf4f390392f3bfe0231c9a924687ae8
2389451262@qq.com <2389451262@qq.com>
2022-12-19 00:35:37 +0800

    feat: implements editor fetch data and update data

--- src/components/Editor/EditorContainer.tsx
+++ src/components/Editor/EditorContainer.tsx
@@ -1,22 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import 'md-editor-rt/lib/style.css'
 import MdEditor from 'md-editor-rt'
+import { useLocation } from 'react-router-dom'
 import { EditorContainer } from './EditorStyled'
+import { getIdbDatabase, IDBGetOneByKey, IDBPutOne } from '../../lib/idb'
+import IDBStoreContext, {
+	DATABASE_NAME,
+	IDBStoreDatabaseType,
+	STORE_NAME,
+} from '../../context/IDBStoreContext'
+import { NoteType } from '../../types/Note'
 
 interface EditorProps {
-	rawText?: string
 	style?: React.CSSProperties
 }
 
-const Editor: React.FC<EditorProps> = ({ rawText, style }) => {
-	const [text, setText] = useState(rawText as string | '')
+const Editor: React.FC<EditorProps> = ({ style }) => {
+	const [noteObject, setNoteObject] = useState<NoteType>({} as NoteType)
+	const [db, setDb] = useState<IDBStoreDatabaseType>()
+	const location = useLocation()
+	const noteDb = useContext(IDBStoreContext)
 
-	useEffect(() => {}, [text])
+	useEffect(() => {
+		// 这里来判断是否有db来重新获取db
+		if (!noteDb) {
+			;(async () => {
+				setDb(await getIdbDatabase(DATABASE_NAME, STORE_NAME, 1))
+			})()
+		} else {
+			setDb(noteDb)
+		}
+	}, [])
+
+	// 获取数据流程
+	useEffect(() => {
+		if (!db) return
+		const id = new URLSearchParams(location.search).get('id')
+		if (!id || !db) {
+			return
+		}
+
+		IDBGetOneByKey({
+			db,
+			storeName: STORE_NAME,
+			key: id,
+		}).then((res) => {
+			if (typeof res === 'object') {
+				setNoteObject(res as NoteType)
+			}
+		})
+	}, [db])
+
+	const saveNoteObject = () => {
+		if (!db) return
+		IDBPutOne({
+			db,
+			storeName: STORE_NAME,
+			key: noteObject.id,
+			value: noteObject,
+		})
+	}
+
 	return (
 		<EditorContainer style={style}>
 			<MdEditor
-				modelValue={text}
-				onChange={setText}
+				modelValue={noteObject.rawText}
+				onChange={(value) => {
+					setNoteObject({
+						...noteObject,
+						rawText: value,
+					})
+				}}
+				onSave={saveNoteObject}
 				toolbars={[
 					'bold',
 					'underline',
