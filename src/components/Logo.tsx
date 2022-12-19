1cdfda4ed1856bdce7eb776967564baab2e98cf0
2389451262@qq.com <2389451262@qq.com>
2022-12-20 00:01:40 +0800

    fix: fix Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,8 @@
+import { BookFilled, HighlightFilled } from '@ant-design/icons'
 import React, { useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import { COLOR_MAIN } from '../lib/antd.color'
 
 interface LogoProps {
 	direction: 'column' | 'row'
@@ -19,6 +22,7 @@ const LogoBlock = styled.div`
 	height: 100%;
 `
 const Logo: React.FC<LogoProps> = ({ direction }) => {
+	const navigate = useNavigate()
 	const isRow = useMemo(() => {
 		return direction === 'row'
 	}, [direction])
@@ -28,9 +32,19 @@ const Logo: React.FC<LogoProps> = ({ direction }) => {
 			style={{
 				width: isRow ? 'auto' : '100%',
 				height: isRow ? '100%' : '64px',
+				cursor: 'pointer',
 			}}
 		>
-			<LogoBlock>NOTE APP</LogoBlock>
+			<LogoBlock onClick={() => navigate('/')}>
+				<HighlightFilled
+					style={{
+						color: `${COLOR_MAIN}`,
+						fontSize: 18,
+						marginRight: 4,
+					}}
+				/>
+				Circle Note
+			</LogoBlock>
 		</LogoContainer>
 	)
 }
