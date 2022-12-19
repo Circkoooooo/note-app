0bcdb3f14cd8c74e0ab97ea4c102ea67c4ea45f0
2389451262@qq.com <2389451262@qq.com>
2022-12-19 19:21:05 +0800

    feat: encapsulate MenuGroup component

---
+++ src/components/MenuGroup/MenuGroup.tsx
@@ -0,0 +1,75 @@
+import React, { ReactNode, useEffect } from 'react'
+import {
+	ElementSpace,
+	ElementSplit,
+	MenuGroupContainer,
+} from './MenuGroupStyled'
+
+interface MenuGroupProps {
+	style?: React.CSSProperties
+	children?: ReactNode[]
+	elementSpace?: number
+	split?: number[]
+}
+
+const MenuGroup: React.FC<MenuGroupProps> = ({
+	style,
+	children,
+	elementSpace = 8,
+	split = [1],
+}) => {
+	useEffect(() => {
+		if (
+			children &&
+			children.length !== split.reduce((pre, val) => pre + val)
+		) {
+			console.warn(
+				`"split" param require a array that sum of all items should be equal to the count of children node. such as "split = [1,1], while the children will be 
+				<>
+					<Node/>
+					<Node/>
+				</>".
+				If not equal, the "split" will be provide [1]`
+			)
+		}
+	}, [])
+
+	const renderSplitChildren = () => {
+		if (
+			split.length === 1 ||
+			children?.length !== split.reduce((pre, val) => pre + val)
+		) {
+			return (
+				<MenuGroupContainer style={{ ...style }}>
+					{children?.map((child, index) => (
+						<ElementSpace key={index} elementSpace={elementSpace}>
+							{child}
+						</ElementSpace>
+					))}
+				</MenuGroupContainer>
+			)
+		}
+
+		const arr = split.map((num, index) => {
+			return children.slice(
+				index - 1 < 0 ? 0 : split[index - 1],
+				(index - 1 < 0 ? 0 : split[index - 1]) + num
+			)
+		})
+		return (
+			<MenuGroupContainer isSpaceBetween>
+				{arr.map((item, index) => (
+					<ElementSplit key={index}>
+						<ElementSpace elementSpace={elementSpace}>
+							{item}
+						</ElementSpace>
+					</ElementSplit>
+				))}
+			</MenuGroupContainer>
+		)
+	}
+
+	return <>{renderSplitChildren()}</>
+}
+
+export default MenuGroup
---
+++ src/components/MenuGroup/MenuGroupStyled.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const MenuGroupContainer = styled.div<{
+	isSpaceBetween?: boolean
+}>`
+	width: 100%;
+	height: 80px;
+	display: flex;
+	align-items: center;
+	padding: 0 14px;
+	border-bottom: 2px solid rgba(0, 0, 0, 0.05);
+	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05);
+	justify-content: ${(props) => {
+		return props.isSpaceBetween ? 'space-between' : ''
+	}};
+	overflow-x: overlay;
+`
+
+/**
+ * controll the margin
+ */
+export const ElementSpace = styled.div<{
+	elementSpace: number
+}>`
+	& > * {
+		margin: ${(props) => props.elementSpace}px;
+	}
+`
+
+/**
+ * controll the split
+ */
+export const ElementSplit = styled.div`
+	display: flex;
+`
